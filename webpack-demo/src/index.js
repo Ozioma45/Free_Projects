4f12b74e1da8d86360234f100b09ead9ba1b699b
ozioma45 <oziomaegole@gmail.com>
2023-12-07 06:03:05 +0100

    getting ready for the next guide/lesson

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -3,29 +3,12 @@ import _ from 'lodash';
 //import my function
 import myName from './myName';
 
-import Icon from './icon.png';
-
-//import css file
-import './style.css';
-
-import Data from './data.xml';
-import Notes from './data.csv';
 function component() {
     const element = document.createElement('div');
   
     //use my function
     element.textContent = myName('Cody');
     element.classList.add('hello')
-
-     // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
-  
-    console.log(Data);
-      console.log(Notes);
-
 
     return element;
   }
